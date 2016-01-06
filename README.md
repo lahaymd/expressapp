b9da5d7c8a64383d082f4ff2cb35112efb037674
lahaymd <lahaymd@gmail.com>
2016-01-05 21:37:03 -0600

    showing nick git

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# myscaffoldingapp
-<<<<<<< e063f2714ee081f14380fd2668beaf583dfd3cb0
+
 express app
 Simple express app with mongodb
 =======
->>>>>>> first commit
-# myscaffoldingapp
-# scaffoldingapp
+
---
+++ public/javascripts/checkform.js
@@ -0,0 +1,21 @@
+  document.addEventListener("submit", function(e) {
+    var text= document.getElementById("inputUserName").value;
+      if(text == "") {
+        alert("Error: Username cannot be blank!");
+        
+        e.preventDefault(); // equivalent to return false
+        return;
+      }
+      re = /^\w+$/;
+      if(!re.test(text)) {
+        alert("Error: Username must contain only letters, numbers and underscores!");
+      
+        e.preventDefault();
+        return;
+      }
+
+    var myForm = document.getElementById("formAddUser");
+    myForm.addEventListener("submit", checkForm, true);
+
+
+  }, false);
