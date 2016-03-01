5129c6a6c526e1a261715a622ee80538f10b61f7
lahaymd <lahaymd@gmail.com>
2016-03-01 11:19:55 -0600

    added logic to bio.jade

--- routes/bio.js
+++ routes/bio.js
@@ -1,5 +1,22 @@
 var mongoose = require('mongoose');
-//var emailRegexp = /.+\@.+\..+/;
+var request = require('request');
+var TIMESPAN_YEAR = 31536000000;
+var TIMESPAN_18_YEARS = 18 * TIMESPAN_YEAR;
+function validate_18_years_old_or_more(date) {
+	return (Date.now() - date.getTime()) > TIMESPAN_18_YEARS; 
+}
+
+
+function twitterHandleExists(handle, done) {
+request('http://twitter.com/' + encodeURIComponent(handle), function(err, res) {
+if (err) { console.error(err); return done(false);
+}
+if (res.statusCode > 299) {
+done(false);
+} else {
+done(true); }
+}); }
+
 var Schema   = mongoose.Schema;
 var Bio = new Schema({
 	username: {type: String, unique: true},
@@ -15,6 +32,38 @@ var Bio = new Schema({
 			}
 
 		}
-	}
+	},
+	gender: {
+		type: String,
+		required: true,
+		uppercase: true,
+		'enum': ['M', 'F']
+	},
+	birthday: { type: Date, validate: [
+	validate_18_years_old_or_more,
+	'You must be 18 years old or more'] },
+	twitter: {
+type: String,
+validate: [twitterHandleExists, 'Please provide a valid twitter handle'], set: function(handle) {
+if (! handle) { return;
+}
+handle = handle.trim();
+if (handle.indexOf('@') === 0) {
+handle = handle.substring(1); }
+return handle; }
+},
+meta: { 
+	created_at: {
+		type: Date,
+		default: Date.now 
+	},
+	updated_at: {
+		type: Date, 	
+		default: Date.now
+	} 
+}
 });
+
+
+
 module.exports = mongoose.model('Bio', Bio);
