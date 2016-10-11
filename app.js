80a2129544447fd9136718fe216dcc3fdc6ce235
lahaymd <lahaymd@gmail.com>
2016-10-11 15:50:31 -0500

    added ngAnimate and animate.css

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static( path.join(__dirname,'node_modules')))
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
