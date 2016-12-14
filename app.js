5aae9d97fb872ea422b1666a2dee64b6015cfeda
lahaymd <lahaymd@gmail.com>
2016-12-13 23:36:00 -0600

    deleted stuff

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var express = require('express');
-var methodOverride = require('method-override');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var util = require('util');
-var multer = require('multer');
 var api = require('./routes/api');
 var routes = require('./routes/index');
 var mongolab = require('./routes/mongolab.api');
@@ -33,19 +31,9 @@ app.use('/static',express.static( path.join(__dirname,'node_modules')))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}));
+
 app.use(cookieParser('anystringoftext'));
-app.use(session({ secret: 'anystringoftext',
-                  saveUninitialized: true,
-                  maxAge: 30000,
-                  resave: true}));
+
 
 
 // Give Views/Layouts direct access to session data.
