bd14999bc11f91a56aab152de55966c768d00b7a
lahaymd <lahaymd@gmail.com>
2016-12-14 00:00:44 -0600

    changed app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+// var session = require('express-session');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var util = require('util');
-var multer = require('multer');
+// var util = require('util');
+// var multer = require('multer');
 var api = require('./routes/api');
 var routes = require('./routes/index');
 var mongolab = require('./routes/mongolab.api');
@@ -33,26 +33,26 @@ app.use('/static',express.static( path.join(__dirname,'node_modules')))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}));
+// app.use(methodOverride(function(req, res){
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     var method = req.body._method
+//     delete req.body._method
+//     return method
+//   }
+// }));
 app.use(cookieParser('anystringoftext'));
-app.use(session({ secret: 'anystringoftext',
-                  saveUninitialized: true,
-                  maxAge: 30000,
-                  resave: true}));
+// app.use(session({ secret: 'anystringoftext',
+//                   saveUninitialized: true,
+//                   maxAge: 30000,
+//                   resave: true}));
 
 
 // Give Views/Layouts direct access to session data.
-  app.use(function(req, res, next){
-    res.locals.session = req.session.user;
-    next();
-  });
+  // app.use(function(req, res, next){
+  //   res.locals.session = req.session.user;
+  //   next();
+  // });
 app.use('/api/users', api);
 app.use('/api/mongolab', mongolab);
 app.get('/', routes.index);
